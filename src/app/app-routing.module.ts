266f91ac4385859da3d6fd2f27a6fc8fb048f50d
buckapi <buckapis@gmail.com>
2023-05-21 17:50:34 -0500

    virtualRoute

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path:'', redirectTo: '/site', pathMatch:'full'},
- { path: 'site', loadChildren: () => import('@components/home/home.module').then(m => m.HomeModule) },
- { path: 'about', loadChildren: () => import('@components/about/about.module').then(m => m.AboutModule) }, 
- { path: 'contact', loadChildren: () => import('@components/contact/contact.module').then(m => m.ContactModule) }];
+ { path: 'site', loadChildren: () => import('@components/home/home.module').then(m => m.HomeModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
