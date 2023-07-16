fd4d96d6240f05cecbfa579b080f19bf89c20393
Buckapi <112070278+buckapi@users.noreply.github.com>
2023-07-16 12:17:55 -0500

    cars

--- src/app/components/home/cars/cars.component.ts
+++ src/app/components/home/cars/cars.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Yeoman } from '@app/services/yeoman.service';
 
 @Component({
   selector: 'app-cars',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public yeoman:Yeoman
+  ) { }
+  
+  setRoute(par:any){
+    let parametro=par;
+  this.yeoman.virtualRoute=parametro;
+  }
 
   ngOnInit(): void {
   }
